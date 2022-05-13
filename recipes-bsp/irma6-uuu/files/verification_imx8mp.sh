6185bae91229e875291860b9037dfc9832524d26
Erik Schumacher <erik.schumacher@iris-sensing.com>
2022-05-13 11:28:36 +0200

    irma6-uuu: Adopt verification.sh to fitimage and LVM

--- recipes-bsp/irma6-uuu/files/verification_imx8mp.sh
+++ recipes-bsp/irma6-uuu/files/verification_imx8mp.sh
@@ -13,23 +13,19 @@ echo "Verify hash of flash.bin on /dev/mmcblk"${mmc}"boot1"
 head -c "$SIZE_FLASHBIN" /dev/mmcblk"${mmc}"boot1 | sha256sum - | cut -d' ' -f1 | grep "$HASH_FLASHBIN" || { echo "Error: Failed to verify hash of flash.bin on /dev/mmcblk"${mmc}"boot1"; exit 1; }
 
 mount   /dev/mmcblk${mmc}p2 /mnt/fat || exit 1
-echo "Verify hash of kernel Image on /dev/mmcblk"${mmc}"p2"
-sha256sum /mnt/fat/Image | cut -d' ' -f1 | grep "$HASH_KERNEL" || { echo "Error: Failed to verify hash of kernel Image on /dev/mmcblk"${mmc}"p2"; exit 1; }
-echo "Verify hash of *.dtb on /dev/mmcblk"${mmc}"p2"
-sha256sum /mnt/fat/imx8mp-irma6r2.dtb | cut -d' ' -f1 | grep $HASH_DEVTREE || { echo "Error: Failed to verify hash of *.dtb on /dev/mmcblk"${mmc}"p2"; exit 1; }
+echo "Verify hash of fitimage on /dev/mmcblk"${mmc}"p2"
+sha256sum /mnt/fat/fitImage.signed | cut -d' ' -f1 | grep "$HASH_FITIMAGE" || { echo "Error: Failed to verify hash of fitimage on /dev/mmcblk"${mmc}"p2"; exit 1; }
 umount /mnt/fat
 sync
 
 mount   /dev/mmcblk${mmc}p3 /mnt/fat || exit 1
-echo "Verify hash of kernel Image on /dev/mmcblk"${mmc}"p3"
-sha256sum /mnt/fat/Image | cut -d' ' -f1 | grep $HASH_KERNEL || { echo "Error: Failed to verify hash of kernel Image on /dev/mmcblk"${mmc}"p3"; exit 1; }
-echo "Verify hash of *.dtb on /dev/mmcblk"${mmc}"p3"
-sha256sum /mnt/fat/imx8mp-irma6r2.dtb | cut -d' ' -f1 | grep $HASH_DEVTREE || { echo "Error: Failed to verify hash of *.dtb on /dev/mmcblk"${mmc}"p3"; exit 1; }
+echo "Verify hash of fitimage on /dev/mmcblk"${mmc}"p3"
+sha256sum /mnt/fat/fitImage.signed | cut -d' ' -f1 | grep $HASH_FITIMAGE || { echo "Error: Failed to verify hash of fitimage on /dev/mmcblk"${mmc}"p3"; exit 1; }
 umount /mnt/fat
 sync
 
-echo "Verify hash of rootfs on /dev/mmcblk"${mmc}"p4"
-head -c "$SIZE_ROOTFS" /dev/mmcblk${mmc}p4 | sha256sum - | cut -d' ' -f1 | grep $HASH_ROOTFS || { echo "Error: Failed to verify hash of rootfs on /dev/mmcblk"${mmc}"p4"; exit 1; }
-echo "Verify hash of rootfs on /dev/mmcblk"${mmc}"p6"
-head -c "$SIZE_ROOTFS" /dev/mmcblk${mmc}p6 | sha256sum - | cut -d' ' -f1 | grep $HASH_ROOTFS || { echo "Error: Failed to verify hash of rootfs on /dev/mmcblk"${mmc}"p6"; exit 1; }
+echo "Verify hash of rootfs on /dev/mapper/irma6lvm-rootfs_a"
+head -c "$SIZE_ROOTFS" /dev/mapper/irma6lvm-rootfs_a | sha256sum - | cut -d' ' -f1 | grep $HASH_ROOTFS || { echo "Error: Failed to verify hash of rootfs on /dev/mapper/irma6lvm-rootfs_a"; exit 1; }
+echo "Verify hash of rootfs on /dev/mapper/irma6lvm-rootfs_b"
+head -c "$SIZE_ROOTFS" /dev/mapper/irma6lvm-rootfs_b | sha256sum - | cut -d' ' -f1 | grep $HASH_ROOTFS || { echo "Error: Failed to verify hash of rootfs on /dev/mapper/irma6lvm-rootfs_b"; exit 1; }
 
