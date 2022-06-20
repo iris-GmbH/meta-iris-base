a352667e3f110c1fb8bd50853d9e12d51232ddf8
¨Martin <martin.koppehel@iris-sensing.net>
2022-06-20 15:17:37 +0200

    Fixup script to use sh instead of bash

--- recipes-support/identity-cert-provision/files/create_identity_cert.sh
+++ recipes-support/identity-cert-provision/files/create_identity_cert.sh
@@ -1,18 +1,16 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 set -e
-set -o pipefail
 
 if [ $# -lt 3 ]; then
 	echo "Usage: $0 PRODUCT_FAMILY SERIAL_NUMBER FILE_DIR"
 	exit 1;
 fi
 
-if [ -z $SSL_CONFIG_FILE ]; then
+if [ -z "$SSL_CONFIG_FILE" ]; then
     SSL_CONFIG_FILE="/etc/openssl/sensor_cert.cnf"
 fi
 
-# TODO: migrate openssl commands to utilize ecc
 echo "Creating key pair..."
 openssl ecparam -name secp521r1 -genkey -noout -out "$3/sensor.key"
 openssl ec -in "$3/sensor.key" -pubout -out "$3/public.pem"
