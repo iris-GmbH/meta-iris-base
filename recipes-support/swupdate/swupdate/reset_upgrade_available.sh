a75fb2bde55649a541f80c9e78f11bdffa9ba31c
Michael Glembotzki <Michael.Glembotzki@iris-sensing.com>
2022-07-22 09:01:45 +0200

    swupdate: Add check if swupdate still runs after update

--- recipes-support/swupdate/swupdate/reset_upgrade_available.sh
+++ recipes-support/swupdate/swupdate/reset_upgrade_available.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 
-PENDING_UPDATE=$(fw_printenv upgrade_available | awk -F'=' '{print $2}')
+# pidofproc()
+. /etc/init.d/functions
 
+# Check if everything is still ok after update on first boot after reboot
+PENDING_UPDATE=$(fw_printenv upgrade_available | awk -F'=' '{print $2}')
 if [ "$PENDING_UPDATE" = "1" ]; then
+	# Check if swupdate is still running
+	if ! pidofproc swupdate; then
+		echo "Error: swupdate is not running"
+		reboot
+		exit 1
+	fi
+
 	# Use a temp file to write u-boot-env's in one go
 	TMP_ENV_FILE="/tmp/reset_update_envs"
 	printf "bootcount=0\nupgrade_available=\nustate=\n" > "$TMP_ENV_FILE"
