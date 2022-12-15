b1f159729f0372def4c17f60fc0f714b9fec2847
Michael Glembotzki <Michael.Glembotzki@iris-sensing.com>
2022-12-15 19:07:23 +0100

    [APC-5725]: Add option for lazy factory reset on next reboot

--- recipes-core/initscripts/files/factory-reset.sh
+++ recipes-core/initscripts/files/factory-reset.sh
@@ -1,18 +1,29 @@
 #!/bin/sh
 
 . /etc/init.d/factory-reset-functions || exit 1
+[ -n "$LAZY_RESET_FLAG" ] || { echo "Error: Filename LAZY_RESET_FLAG is not set"; exit 1; }
 
 FORCE=0
-while getopts ":fr" options; do
+LAZY=0
+
+while getopts ":lf" options; do
     case "${options}" in
         f) FORCE=1 ;;
-        *) echo "Usage: $0 [-f]" >&2; exit 1 ;;
+        l) LAZY=1 ;;
+        *) echo "Usage: $0 [-f] [-l]" >&2; exit 1 ;;
     esac
 done
 
-# Check if the factory reset is forced
-if [ "$FORCE" -eq 1 ]; then
+# Check if the factory reset is forced or if the reset flag is set
+if [ "$FORCE" -eq 1 ] || [ -f "$LAZY_RESET_FLAG" ]; then
     factory_reset
+    rm -f "$LAZY_RESET_FLAG"
+    exit 0
+fi
+
+# Touch a file to perform a factory reset on the next reboot
+if [ "$LAZY" -eq 1 ]; then
+    touch "$LAZY_RESET_FLAG"
     exit 0
 fi
 
