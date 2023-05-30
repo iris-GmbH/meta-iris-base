6b10a14cabb7fc44f12057b4171b5ed6ba78b92f
an.tran <An.Tran@iris-sensing.com>
2023-05-30 11:01:54 +0200

    [APC-6513] Added action scripts and modified yocto recipes

---
+++ recipes-core/busybox/busybox/sc57x/starting_ntpdaemon.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+echo "NTP server list: $1";
+
+NTPDIR="/bin/busybox"
+
+ntpsrv_list=$1
+
+
+if [ -n "$ntpsrv_list" ] ; then
+	echo "The ntpsrv list is not empty."
+
+	for i in $ntpsrv_list; do
+		echo "start NTP daemon for ip: $i"
+		#-g: This option allows ntpd to make large time adjustments, even if the system time is significantly different from the time reported by the NTP server.
+		#-n: This option is used to run ntpd in the foreground without forking into the background.
+		# It can be useful for seeing the output and status messages directly on the terminal.
+		#-p: The -p option is used to specify the address of the NTP server to query.
+		# &: This ampersand symbol at the end of the command is used to run the command in the background.
+		# It allows the script or program to continue executing without waiting for the ntpd command to finish.
+		$NTPDIR ntpd -g -n -p "$i" &
+		done
+	else
+		echo "The ntpsrv list is empty."
+	fi
---
+++ recipes-core/busybox/busybox/starting_ntpdaemon.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+echo "NTP server list for starting_ntpdaemon.sh: $1";
+
+ntpsrv_list=$1
+
+if [ -n "$ntpsrv_list" ] ; then
+	echo "The ntpsrv list is not empty."
+
+	# Iterate over the server addresses and pass them to chronyc
+	for server in $ntpsrv_list; do
+		echo "Add ntp server $server."
+		sudo chronyc add server "$server" prefer
+	done
+else
+		echo "The ntpsrv list is empty."
+fi
