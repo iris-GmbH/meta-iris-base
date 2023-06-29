2dd55db7e96c6d041a5a58462deb9f5b8c14a394
an.tran <An.Tran@iris-sensing.com>
2023-06-29 10:14:00 +0200

    [APC-6765] checking already running ntpd (R1) reload sources (R2)

--- recipes-core/busybox/busybox/timeservice_dhcp_option_42.sh
+++ recipes-core/busybox/busybox/timeservice_dhcp_option_42.sh
@@ -13,15 +13,18 @@ if [ -n "$ntpsrv_list" ] ; then
 	
 	if [ -e "$CHRONY_DHCP_PATH_FILE" ]; then
   		rm CHRONY_DHCP_PATH_FILE
-	else
-  		touch "$CHRONY_DHCP_PATH_FILE"
 	fi
+  	touch "$CHRONY_DHCP_PATH_FILE"
 	
 	# Iterate over the server addresses and write them to the file for passing to chronyc later
 	for server in $ntpsrv_list; do
 		echo "writing $server to $CHRONY_DHCP_PATH_FILE - Script timeservice_dhcp_option_42.sh"
 		echo "server" "$server" "iburst">> "$CHRONY_DHCP_PATH_FILE"
 	done
+	
+	# Run chronyc reload sources to synchronize system time
+	chronyc reload sources
+
 else
 	echo "The ntpsrv list from dhcp server via option 42 is empty - nothing to do!."
 fi
