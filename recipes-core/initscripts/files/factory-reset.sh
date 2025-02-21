cf8458625b8e7884d02d19d21e8480d2841482cc
Michael Glembotzki <Michael.Glembotzki@iris-sensing.com>
2025-02-21 10:35:08 +0100

    [MARE-200] factory-reset: Implement Matrix UP factory reset via WebAPI

--- recipes-core/initscripts/files/factory-reset.sh
+++ recipes-core/initscripts/files/factory-reset.sh
@@ -25,7 +25,14 @@ if [ "$LAZY" -eq 1 ]; then
     exit 0
 fi
 
-# Skip rj45 short check on nfs boot
-if ! grep -q '/dev/nfs' /proc/cmdline; then
+if grep -q '/dev/nfs' /proc/cmdline; then
+    echo "Skip factory reset short check on NFS boot!"
+    exit 0
+fi
+
+if [ "$(cat /sys/class/net/eth0/carrier)" -eq 0 ]; then
     short_detected && factory_reset || exit 1
+else
+    echo "Skip factory reset short check. Link is up!"
+    exit 0
 fi
